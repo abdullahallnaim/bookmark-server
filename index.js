e6b34de519b48dc98f7184c6c679768504f4f3f3
naim-007 <naim1823007@gmail.stud.kuet.ac.bd>
2021-03-28 09:23:56 +0600

    done

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = 5000
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 require('dotenv').config()
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(fileUpload());
+// app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5omi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
